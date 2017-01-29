92dc38871e2325acc562896aa7b51e69338f2ec9
xavier poh <xvrpke@gmail.com>
2017-01-29 16:03:14 +0800

    add createCards function to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,21 @@
-var cardOne = 'queen'
-var cardTwo = 'queen'
-var cardThree = 'king'
-var cardFour = 'king'
+// var cardOne = 'queen'
+// var cardTwo = 'queen'
+// var cardThree = 'king'
+// var cardFour = 'king'
+//
+// if (cardTwo === cardFour) {
+//   alert('You found a match!')
+// } else {
+//   alert('Sorry, try again.')
+// }
 
-if (cardTwo === cardFour) {
-  alert('You found a match!')
-} else {
-  alert('Sorry, try again.')
+function createCards () {
+  var gameBoard = document.getElementById('game-board')
+  for (var i = 0; i < 4; i++) {
+    var card = document.createElement('div')
+    card.className = 'card'
+    gameBoard.appendChild(card)
+  }
 }
+
+createCards()
